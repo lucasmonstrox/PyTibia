4749de1ab121cb5b9771bf54ea2cf66d9df74428
lucasmonstro <joao.galiano.silva@gmail.com>
2021-12-13 01:24:59 +0000

    :sparkles: add pathding character walking example

---
+++ pathfinding.py
@@ -0,0 +1,32 @@
+import numpy as np
+from time import sleep, time
+from skimage.graph import route_through_array
+
+image = np.array([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+
+
+def main():
+    loop_time = time()
+    while True:
+        # Forbid diagonal steps
+        a = route_through_array(image, [0, 0], [3, 3], fully_connected=False)
+        timef = (time() - loop_time)
+        timef = timef if timef else 1
+        fps = 1 / timef
+        print('FPS {}'.format(fps))
+        loop_time = time()
+
+
+if __name__ == '__main__':
+    main()
