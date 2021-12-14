08be8c24c9c05bdf555a18a75c15d596c8545b7f
lucasmonstro <joao.galiano.silva@gmail.com>
2021-12-14 00:50:13 +0000

    :alembic: add new hud experiments

--- hud/hud.py
+++ hud/hud.py
@@ -1,38 +1,44 @@
-
-
-import cv2
-import d3dshot
-from battle import battleList
-from player import player
-from time import time
 import cupy as cp
-import math
-import numpy as np
-import pyautogui
-import pygetwindow as gw
-from utils import utils
+import cv2
 
 
-playable = cp.array(cv2.imread(
-    'playable.png', cv2.IMREAD_GRAYSCALE), dtype=cp.uint8)
-screenshot = np.array(cv2.imread(
-    'screenshot.png', cv2.IMREAD_GRAYSCALE), dtype=cp.uint8)
-upperBar = cp.array(cv2.imread(
-    'blackbar.png', cv2.IMREAD_GRAYSCALE), dtype=cp.uint8)
+upperBar = cp.array(cv2.imread('blackbar.png', cv2.IMREAD_GRAYSCALE)).flatten()
 
 
-def getCreaturesPositions(arr, seq):
+def getCreaturesBars(blackPixels):
+    blackPixelsIndexes = cp.nonzero(blackPixels == 0)[0]
+    f = cp.where(blackPixelsIndexes <= blackPixels.size - upperBar.size)[0]
+    blackPixelsIndexes = cp.take(blackPixelsIndexes, f)
+    noBlackPixels = len(blackPixelsIndexes) == 0
+    if noBlackPixels:
+        return cp.array([])
+    x = cp.arange(upperBar.size)
+    x = cp.broadcast_to(x, (blackPixelsIndexes.size, upperBar.size))
+    y = blackPixelsIndexes.reshape(blackPixelsIndexes.size, 1)
+    y = cp.pad(y, ((0, 0), (0, upperBar.size - 1)), 'maximum')
+    final = cp.take(blackPixels, x + y)
+    possibleCreatures = cp.nonzero((final == upperBar).all(1))[0]
+    hasNoCreatures = len(possibleCreatures) == 0
+    if hasNoCreatures:
+        return cp.array([])
+    creatures = cp.take(blackPixelsIndexes, possibleCreatures)
+    return creatures
+
+
+def getCreaturesBars_deprecated(arr, seq):
     r_seq = cp.arange(seq.size)
     # TODO: initialize values directly
     arr = arr[cp.arange(arr.size - seq.size + 1)[:, None] + r_seq]
     possibleMonsters = cp.nonzero((arr == seq).all(1))[0]
-    possibleMonsters = cp.where(
-        cp.logical_and(
-            cp.logical_and(arr[possibleMonsters + 480] == 0,
-                           arr[possibleMonsters + 960] == 0),
-            arr[possibleMonsters + 1440] == 0
-        ),
-        0, possibleMonsters
-    )
+    if len(possibleMonsters) == 0:
+        return cp.array([])
+    # possibleMonsters = cp.where(
+    #     cp.logical_and(
+    #         cp.logical_and(arr[possibleMonsters + 480] == 0,
+    #                        arr[possibleMonsters + 960] == 0),
+    #         arr[possibleMonsters + 1440] == 0
+    #     ),
+    #     0, possibleMonsters
+    # )
     possibleMonsters = possibleMonsters[possibleMonsters != 0]
     return possibleMonsters
