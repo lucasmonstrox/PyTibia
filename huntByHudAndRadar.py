f6e8ae92138f1e4ab610bc8876035c90f07f135e
lucasmonstro <joao.galiano.silva@gmail.com>
2022-01-17 11:40:05 +0000

    :alembic: improve hunt by hud/radar

--- huntByHudAndRadar.py
+++ huntByHudAndRadar.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 from time import sleep
 from battleList import battleList
@@ -17,12 +16,24 @@ waypoints = [
 ]
 
 
+def getTemporaryCoordinate(screenshot):
+    floorLevel = radar.getFloorLevel(screenshot)
+    radarToolsPos = radar.getRadarToolsPos(screenshot)
+    radarImage = radar.getRadarImage(screenshot, radarToolsPos)
+    coordinate = radar.getCoordinate(floorLevel, radarImage)
+    return coordinate
+
+
 def main():
-    screenshot = utils.loadImgAsArray('screenshot.png')
     currentWaypointIndex = 0
     souldRetrySameWaypoint = True
     while True:
         screenshot = utils.getScreenshot()
+        coordinate = getTemporaryCoordinate(screenshot)
+        coordinateIsEmpty = coordinate is None
+        if coordinateIsEmpty:
+            print('Cannot get coordinate')
+            continue
         hudCoordinates = hud.getCoordinates(screenshot)
         hudImg = hud.getImgByCoordinates(screenshot, hudCoordinates)
         emptyHud = hudImg is None
@@ -40,7 +51,6 @@ def main():
             if battleListCreatures["isAttackingAnyCreature"]:
                 print('already attacking any creature 1')
                 continue
-            player.stop()
             hudImgFlattened = hudImg.flatten()
             creaturesBars = hud.getCreaturesBars(hudImgFlattened)
             hasHudCreaturesBars = len(creaturesBars) > 0
@@ -49,25 +59,28 @@ def main():
                     hudImg, creaturesBars, battleListCreatures)
                 hasHudCreatures = len(hudCreatures) > 0
                 if hasHudCreatures:
-                    print('going to attack creature')
-                    hud.rightClickSlot(hudCreatures[0]["slot"], hudCoordinates)
-                    sleep(2)
-                    continue
+                    shortestCreatureIndex = hud.getClosestCreatures(hudCreatures)
+                    if shortestCreatureIndex is not None:
+                        x = shortestCreatureIndex % 15
+                        y = shortestCreatureIndex // 15
+                        player.stop(1)
+                        screenshot = utils.getScreenshot()
+                        temporaryCoordinate = getTemporaryCoordinate(screenshot)
+                        if coordinate != temporaryCoordinate:
+                            xDifference = coordinate[0] - temporaryCoordinate[0]
+                            yDifference = coordinate[1] - temporaryCoordinate[1]
+                            x += xDifference
+                            y += yDifference
+                        hud.rightClickSlot((x, y), hudCoordinates)
+                        sleep(1)
+                        continue
                 else:
                     print('There is no hud creatures')
             else:
                 print('There is no hud creatures bars')
-        floorLevel = radar.getFloorLevel(screenshot)
-        radarToolsPos = radar.getRadarToolsPos(screenshot)
-        radarImage = radar.getRadarImage(screenshot, radarToolsPos)
-        coordinate = radar.getCoordinate(floorLevel, radarImage)
-        coordinateIsEmpty = coordinate is None
-        if coordinateIsEmpty:
-            print('Cannot get coordinate')
-            continue
         if radar.isNearToCoordinate(
                 coordinate, waypoints[currentWaypointIndex]["coordinate"]):
-            player.stop()
+            player.stop(1)
             currentWaypointIndex = 0 if currentWaypointIndex == len(
                 waypoints) - 1 else currentWaypointIndex + 1
             radar.goToCoordinateByRadarClick(
@@ -75,7 +88,7 @@ def main():
             continue
         if souldRetrySameWaypoint:
             souldRetrySameWaypoint = False
-            player.stop()
+            player.stop(1)
             radar.goToCoordinateByRadarClick(
                 screenshot, coordinate, waypoints[currentWaypointIndex]["coordinate"])
             continue
