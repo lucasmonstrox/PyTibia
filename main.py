f43d448ae95949112b8935870ef42ba9d74a9e88
lucasmonstro <joao.galiano.silva@gmail.com>
2022-07-04 03:12:32 +0100

    :recycle: add lamassu waypoints

--- main.py
+++ main.py
@@ -20,69 +20,77 @@ waypointType = np.dtype([
     ('coordinate', np.uint32, (3,)),
     ('tolerance', np.uint8)
 ])
+# waypoints = np.array([
+#     ('floor', (33085, 32788, 7), 0),
+#     ('ramp', (33085, 32786, 6), 0),
+#     ('floor', (33085, 32785, 6), 0),
+#     ('ramp', (33085, 32783, 7), 0),
+#     ('floor', (33038, 32810, 7), 0),
+#     ('ramp', (33038, 32808, 6), 0),
+#     ('floor', (33033, 32801, 6), 0),
+#     ('ramp', (33033, 32799, 5), 0),
+#     ('floor', (33032, 32779, 5), 0),
+#     ('ramp', (33032, 32777, 4), 0),
+#     ('floor', (33032, 32786, 4), 0),
+#     ('ramp', (33032, 32788, 3), 0),
+#     ('floor', (33031, 32788, 3), 0),
+#     ('ramp', (33029, 32788, 2), 0),
+#     ('floor', (33029, 32781, 2), 0),
+#     ('ramp', (33029, 32779, 1), 0),
+#     ('floor', (33029, 32776, 1), 0),
+#     ('ramp', (33029, 32774, 0), 0),
+#     ('floor', (33030, 32756, 0), 0),
+#     ('ramp', (33032, 32756, 1), 0),
+#     ('floor', (33021, 32746, 1), 0),
+#     ('ramp', (33019, 32746, 2), 0),
+#     ('floor', (33016, 32745, 2), 0),
+#     ('ramp', (33016, 32743, 3), 0),
+#     ('floor', (33010, 32727, 3), 0),
+#     ('ramp', (33010, 32725, 4), 0),
+#     ('floor', (32999, 32703, 4), 0),
+#     ('ramp', (32999, 32705, 5), 0),
+#     ('floor', (32998, 32709, 5), 0),
+#     ('ramp', (32998, 32711, 6), 0),
+#     ('floor', (32982, 32715, 6), 0),
+#     ('ramp', (32982, 32717, 7), 0),
+#     ('floor', (32953, 32769, 7), 0),
+    
+#     ('floor', (32953, 32785, 7), 0),
+#     ('floor', (32959, 32785, 7), 0),
+    
+#     ('floor', (33004, 32750, 7), 0),
+#     ('ramp', (33006, 32750, 6), 0),
+#     ('floor', (33015, 32749, 6), 0),
+#     ('ramp', (33015, 32751, 5), 0),
+#     ('ramp', (33015, 32753, 4), 0),
+#     ('floor', (33009, 32770, 4), 0),
+#     ('ramp', (33009, 32772, 5), 0),
+#     ('floor', (33004, 32765, 5), 0),
+#     ('ramp', (33004, 32767, 6), 0),
+#     ('floor', (33004, 32770, 6), 0),
+#     ('ramp', (33004, 32772, 7), 0),
+#     ('floor', (33004, 32772, 7), 10),
+#     ('floor', (32994, 32815, 7), 10),
+#     ('floor', (33003, 32831, 7), 10),
+#     ('floor', (32975, 32802, 7), 10),
+#     ('floor', (32957, 32824, 7), 10),
+#     ('floor', (32978, 32832, 7), 10),
+#     ('floor', (32981, 32857, 7), 10),
+#     ('floor', (32961, 32861, 7), 10),
+#     ('floor', (32958, 32834, 7), 10),
+#     ('floor', (32987, 32799, 7), 10),
+#     # ('floor', (32981, 32795, 7)),
+#     # ('floor', (32962, 32814, 7)),
+#     # ('floor', (32996, 32805, 7)),
+#     # ('floor', (33005, 32783, 7)),
+# ], dtype=waypointType)
+
 waypoints = np.array([
-    ('floor', (33085, 32788, 7), 0),
-    ('ramp', (33085, 32786, 6), 0),
-    ('floor', (33085, 32785, 6), 0),
-    ('ramp', (33085, 32783, 7), 0),
-    ('floor', (33038, 32810, 7), 0),
-    ('ramp', (33038, 32808, 6), 0),
-    ('floor', (33033, 32801, 6), 0),
-    ('ramp', (33033, 32799, 5), 0),
-    ('floor', (33032, 32779, 5), 0),
-    ('ramp', (33032, 32777, 4), 0),
-    ('floor', (33032, 32786, 4), 0),
-    ('ramp', (33032, 32788, 3), 0),
-    ('floor', (33031, 32788, 3), 0),
-    ('ramp', (33029, 32788, 2), 0),
-    ('floor', (33029, 32781, 2), 0),
-    ('ramp', (33029, 32779, 1), 0),
-    ('floor', (33029, 32776, 1), 0),
-    ('ramp', (33029, 32774, 0), 0),
-    ('floor', (33030, 32756, 0), 0),
-    ('ramp', (33032, 32756, 1), 0),
-    ('floor', (33021, 32746, 1), 0),
-    ('ramp', (33019, 32746, 2), 0),
-    ('floor', (33016, 32745, 2), 0),
-    ('ramp', (33016, 32743, 3), 0),
-    ('floor', (33010, 32727, 3), 0),
-    ('ramp', (33010, 32725, 4), 0),
-    ('floor', (32999, 32703, 4), 0),
-    ('ramp', (32999, 32705, 5), 0),
-    ('floor', (32998, 32709, 5), 0),
-    ('ramp', (32998, 32711, 6), 0),
-    ('floor', (32982, 32715, 6), 0),
-    ('ramp', (32982, 32717, 7), 0),
-    ('floor', (32953, 32769, 7), 0),
-    
-    ('floor', (32953, 32785, 7), 0),
-    ('floor', (32959, 32785, 7), 0),
-    
-    ('floor', (33004, 32750, 7), 0),
-    ('ramp', (33006, 32750, 6), 0),
-    ('floor', (33015, 32749, 6), 0),
-    ('ramp', (33015, 32751, 5), 0),
-    ('ramp', (33015, 32753, 4), 0),
-    ('floor', (33009, 32770, 4), 0),
-    ('ramp', (33009, 32772, 5), 0),
-    ('floor', (33004, 32765, 5), 0),
-    ('ramp', (33004, 32767, 6), 0),
-    ('floor', (33004, 32770, 6), 0),
-    ('ramp', (33004, 32772, 7), 0),
-    ('floor', (33004, 32772, 7), 10),
-    ('floor', (32994, 32815, 7), 10),
-    ('floor', (33003, 32831, 7), 10),
-    ('floor', (32975, 32802, 7), 10),
-    ('floor', (32957, 32824, 7), 10),
-    ('floor', (32978, 32832, 7), 10),
-    ('floor', (32981, 32857, 7), 10),
-    ('floor', (32961, 32861, 7), 10),
-    ('floor', (32958, 32834, 7), 10),
-    ('floor', (32987, 32799, 7), 10),
-    # ('floor', (32981, 32795, 7)),
-    # ('floor', (32962, 32814, 7)),
-    # ('floor', (32996, 32805, 7)),
-    # ('floor', (33005, 32783, 7)),
+    ('floor', (33871, 31457, 7), 0),
+    ('floor', (33875, 31441, 7), 0),
+    ('floor', (33850, 31457, 7), 0),
+    ('floor', (33828, 31473, 7), 0),
+    ('floor', (33842, 31500, 7), 0),
 ], dtype=waypointType)
 waypointIndex = None
 shouldIgnoreTargetAndGoToNextWaypoint = False
@@ -136,12 +144,8 @@ def handleCavebot(screenshot, playerCoordinate, battleListCreatures):
         print('has no closest creature')
         shouldIgnoreTargetAndGoToNextWaypoint = True
         return
-    hasOnlyOneCreature = len(hudCreatures) == 1
-    if hasOnlyOneCreature:
-        battleList.attackSlot(screenshot, 0)
-    else:
-        (x, y) = closestCreature['windowCoordinate']
-        utils.rightClick(x, y)
+    (x, y) = closestCreature['windowCoordinate']
+    utils.rightClick(x, y)
     time.sleep(0.25)
 
 
@@ -207,13 +211,13 @@ def handleWaypoints(result):
         goToWaypoint(screenshot, currentWaypoint, coordinate)
 
 
-def healingObserver(healthPercentage, manaPercentage):
+def handleHealing(healthPercentage, manaPercentage):
     percentageToHealWithManaPotion = 65
     percentageToHealWithPotion = 50
     percentageToHealWithSpell = 75
-    spellHotkey = 'f1'
+    spellHotkey = 'f3'
     manaPotionHotkey = 'f2'
-    healthPotionHotkey = 'f3'
+    healthPotionHotkey = 'f1'
     shouldHealWithHealthPotion = healthPercentage < percentageToHealWithPotion
     if shouldHealWithHealthPotion:
         print('healing with health potion...')
@@ -226,11 +230,11 @@ def healingObserver(healthPercentage, manaPercentage):
         utils.press(manaPotionHotkey)
         time.sleep(0.25)
         return
-    shouldHealWithSpell = healthPercentage < percentageToHealWithSpell or manaPercentage > 90
-    if shouldHealWithSpell:
-        print('healing with spell...')
-        utils.press(spellHotkey)
-        time.sleep(0.25)
+    # shouldHealWithSpell = healthPercentage < percentageToHealWithSpell or manaPercentage > 90
+    # if shouldHealWithSpell:
+    #     print('healing with spell...')
+    #     utils.press(spellHotkey)
+    #     time.sleep(0.25)
 
 
 def getWindow():
@@ -260,7 +264,9 @@ def handleWindow(_):
 def handleSpell(screenshot, battleListCreatures):
     hudCreatures = hud.getCreatures(screenshot, battleListCreatures)
     nearestCreaturesCount = hud.getNearestCreaturesCount(hudCreatures)
+    print('nearestCreaturesCount', nearestCreaturesCount)
     shouldSpell = nearestCreaturesCount > 1
+    print('shouldSpell', shouldSpell)
     if shouldSpell:
         utils.press('F3')
 
@@ -287,7 +293,7 @@ def main():
         operators.map(lambda result: [result[0], result[1], battleList.getCreatures(result[0])]),
     )
     cavebotObserver = battlelistObserver.pipe(
-        operators.filter(lambda result: result != None and len(result[2]) > 0 and not battleList.isAttackingCreature(result[2]) and shouldIgnoreTargetAndGoToNextWaypoint == False),
+        operators.filter(lambda result: result[2] != None and len(result[2]) > 0 and not battleList.isAttackingCreature(result[2]) and shouldIgnoreTargetAndGoToNextWaypoint == False),
         operators.subscribe_on(threadPoolScheduler)
     )
     cavebotObserver.subscribe(
@@ -301,13 +307,13 @@ def main():
     spellObserver = battlelistObserver.pipe(
         operators.subscribe_on(threadPoolScheduler)
     )
-    # spellObserver.subscribe(lambda result: handleSpell(result[0], result[2]))
+    spellObserver.subscribe(lambda result: handleSpell(result[0], result[2]))
     healingObserver = fpsWithScreenshot.pipe(
         operators.map(lambda screenshot: (screenshot, player.getHealthPercentage(screenshot))),
         operators.map(lambda result: (result[1], player.getManaPercentage(result[0]))),
         operators.subscribe_on(threadPoolScheduler)
     )
-    # healingObserver.subscribe(lambda result: healing.healingObserver(result[0], result[1]))
+    healingObserver.subscribe(lambda result: handleHealing(result[0], result[1]))
     input("Press Enter key to exit...")
 
 
