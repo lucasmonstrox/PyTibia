c1860a1b7b74a5d77b6c414ba2410290d9954cdc
Augusto Cesar Rossi de Mattos <augustocrmattos@hotmail.com>
2022-07-28 13:44:59 -0300

    Implementing new waypoint structure Implemening method to click on hud with hud coordinates Implemented AStar search

--- utils/mouse.py
+++ utils/mouse.py
@@ -1,4 +1,6 @@
+from time import sleep
 import pyautogui
+from hud.core import getCoordinates
 
 
 def rightClick(x, y):
@@ -18,3 +20,15 @@ def mouseDrag(x1, y1, x2, y2):
     pyautogui.mouseDown()
     pyautogui.moveTo(x2, y2, 0.5)
     pyautogui.mouseUp()
+
+
+def clickOnHud(screenshot, currentCoordinate, clickCoordinate):
+    (hudX, hudY, hudW, hudH) = getCoordinates(screenshot)
+    (x1, y1, z1) = currentCoordinate
+    (x2, y2, z2) = clickCoordinate
+    if abs(x1-x2) > 7 or abs(y1-y2) > 5 or z1 != z2:
+        return
+    (centerX, centerY) = (hudW/2, hudH/2)
+    (diffX, diffY) = ((x1-x2)*(hudH/11), (y1-y2)*(hudH/11))
+    pyautogui.click(hudX+centerX-diffX, hudY+centerY-diffY)
+    sleep(0.25)
