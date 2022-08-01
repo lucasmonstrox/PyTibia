d29de7ceebdb98f04eefd9ea5958f8e9debe2ce3
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-01 04:20:51 +0100

    :memo: add battleList todos

--- battleList/core.py
+++ battleList/core.py
@@ -4,23 +4,29 @@ from battleList import config, types
 import utils.core, utils.image, utils.mouse
 
 
+# TODO: add unit tests
+# TODO: improve clean code
 def attackSlot(screenshot, slot):
     pos = getContainerTopBarPos(screenshot)
-    x = pos[0] + 20
+    x = pos[0] + config.slot['height']
     y = pos[1] + 13 + (slot * 22) + 22 // 2
     utils.mouse.leftClick(x, y)
 
 
+# TODO: add unit tests
 @utils.core.cacheObjectPos
 def getContainerBottomBarPos(img):
     return utils.core.locate(img, config.container["bottomBarImg"])
 
 
+# TODO: add unit tests
 @utils.core.cacheObjectPos
 def getContainerTopBarPos(img):
     return utils.core.locate(img, config.container['topBarImg'])
 
 
+# TODO: add unit tests
+# TODO: improve clean code
 def getContent(screenshot):
     containerTopBarPos = getContainerTopBarPos(screenshot)
     cannotGetContainerTopBarPos = containerTopBarPos is None
@@ -38,12 +44,16 @@ def getContent(screenshot):
     return content
 
 
+# TODO: add unit tests
+# TODO: improve clean code
 def getCreatureNameImg(slotImg):
     creatureNameImg = slotImg[3:11 + 3, 23:23 + 131]
     creatureNameImg = utils.image.convertGraysToBlack(creatureNameImg)
     return creatureNameImg
 
 
+# TODO: add unit tests
+# TODO: improve clean code
 def getCreatureSlotImg(content, slot):
     isFirstSlot = slot == 0
     startingY = 0 if isFirstSlot else slot * \
@@ -53,6 +63,7 @@ def getCreatureSlotImg(content, slot):
     return slotImg
 
 
+# TODO: add unit tests
 def getCreatureFromSlot(content, slot):
     slotImg = getCreatureSlotImg(content, slot)
     isBeingAttacked = isCreatureBeingAttacked(slotImg)
@@ -65,50 +76,58 @@ def getCreatureFromSlot(content, slot):
     return (creatureName, isBeingAttacked)
 
 
+# TODO: add unit tests
 def getCreatures(screenshot):
     content = getContent(screenshot)
     cannotGetContent = content is None
     if cannotGetContent:
         return None
-    contentIsTooSmall = content.shape[0] < config.slot["height"]
-    if contentIsTooSmall:
-        raise None
+    contentTooSmall = content.shape[0] < config.slot["height"]
+    if contentTooSmall:
+        return None
     content = unhighlightName(content)
-    slotsCount = getSlotsCount(content)
+    slotsCount = getFilledSlotsCount(content)
     creatures = np.array([getCreatureFromSlot(content, creature)
                          for creature in np.arange(slotsCount)], dtype=types.creatureType)
     return creatures
 
 
-def getSlotsCount(content):
+# TODO: add unit tests
+# TODO: improve clean code
+def getFilledSlotsCount(content):
     content = np.ravel(content[:, 23:24])
-    possibleCreatureNames = np.nonzero(
+    paintedPixelsIndexes = np.nonzero(
         np.where(
             content == config.creatures["namePixelColor"],
             True,
             False
         )
     )[0]
-    hasNoFilledSlots = len(possibleCreatureNames) == 0
+    hasNoFilledSlots = len(paintedPixelsIndexes) == 0
     if hasNoFilledSlots:
         return 0
-    lastCreatureIndex = possibleCreatureNames[len(possibleCreatureNames) - 1]
-    filledSlots = math.ceil(
+    lastCreatureIndex = paintedPixelsIndexes[len(paintedPixelsIndexes) - 1]
+    slotsCount = math.ceil(
         lastCreatureIndex / (config.slot["height"] + config.slot["gap"]))
-    return filledSlots
+    return slotsCount
 
 
-def isCreatureBeingAttacked(slotImg):
-    upperCreatureBorder = slotImg[0:1, 0:19].flatten()
-    isBeingAttacked = np.all(np.logical_or(
-        upperCreatureBorder == 76, upperCreatureBorder == 166))
-    return isBeingAttacked
-
-
+# TODO: add unit tests
 def isAttackingSomeCreature(creatures):
-    return np.any(creatures['isBeingAttacked'] == True)
+    isAttackingSomeCreature = np.any(creatures['isBeingAttacked'] == True)
+    return isAttackingSomeCreature
 
 
+# TODO: add unit tests
+# TODO: improve clean code
+def isCreatureBeingAttacked(slotImg):
+    upperCreatureIconBorder = slotImg[0:1, 0:19].flatten()
+    isCreatureBeingAttacked = np.all(np.logical_or(
+        upperCreatureIconBorder == 76, upperCreatureIconBorder == 166))
+    return isCreatureBeingAttacked
+
+
+# TODO: add unit tests
 def unhighlightName(creatureNameImg):
     return np.where(
         creatureNameImg == config.creatures["highlightedNamePixelColor"],
