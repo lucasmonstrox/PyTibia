bdf4c474614fd058942006c2e8a3c6e9ea5784bf
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-14 07:20:19 +0100

    :recycle: remove unused typing

--- battleList/typing.py
+++ battleList/typing.py
@@ -6,6 +6,5 @@ from typing import Any
 creatureType = np.dtype([('name', np.str_, 64), ('isBeingAttacked', np.bool_)])
 CREATURE = Structure["name: Str, isBeingAttacked: Bool"]
 CREATURE_LIST = NDArray[Any, CREATURE]
-UINT8_VECTOR = NDArray[Any, Int8]
 CREATURE_NAME_IMG = NDArray[Shape["11, 131"], Int8]
 SLOT_IMG = NDArray[Shape["20, 156"], Int8]
