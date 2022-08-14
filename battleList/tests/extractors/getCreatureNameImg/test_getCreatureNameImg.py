44511d1c15bc19feabb78a2928209424b8fbedc4
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-14 08:04:19 +0100

    :bug: fix wrong path in test

--- battleList/tests/extractors/getCreatureNameImg/test_getCreatureNameImg.py
+++ battleList/tests/extractors/getCreatureNameImg/test_getCreatureNameImg.py
@@ -9,6 +9,7 @@ currentPath = pathlib.Path(__file__).parent.resolve()
 
 def test_should_get_creature_name_img():
     slotImg = utils.image.loadAsArray(f'{currentPath}/slotImg.png')
-    ratNameImg = utils.image.loadAsArray(f'{currentPath}/images/monsters/Rat.png')
+    # TODO: import path automatically
+    ratNameImg = utils.image.loadAsArray('battleList/images/monsters/Rat.png')
     creatureNameImg = getCreatureNameImg(slotImg)
     np.testing.assert_array_equal(creatureNameImg, ratNameImg)
