47c6d3ea035905641d9061e35e47d0255d585a86
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-18 11:55:52 +0100

    :white_check_mark: add battleList tests to creatures with name A

--- battleList/tests/extractors/getCreatureNameImg/test_getCreatureNameImg.py
+++ battleList/tests/extractors/getCreatureNameImg/test_getCreatureNameImg.py
@@ -84,3 +84,218 @@ def test_should_assert_Aggressive_Matter():
 def test_should_assert_Agrestic_Chicken():
     creatureNameImg, nameImg = getImages('Agrestic Chicken')
     np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Alptramun():
+    creatureNameImg, nameImg = getImages('Alptramun')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Amazon():
+    creatureNameImg, nameImg = getImages('Amazon')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_An_Astral_Glyph():
+    creatureNameImg, nameImg = getImages('An Astral Glyph')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_An_Observer_Eye():
+    creatureNameImg, nameImg = getImages('An Observer Eye')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ancient_Lion_Archer():
+    creatureNameImg, nameImg = getImages('Ancient Lion Archer')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ancient_Lion_Knight():
+    creatureNameImg, nameImg = getImages('Ancient Lion Knight')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ancient_Lion_Warlock():
+    creatureNameImg, nameImg = getImages('Ancient Lion Warlock')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ancient_Scarab():
+    creatureNameImg, nameImg = getImages('Ancient Scarab')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ancient_Spawn_Of_Morgathla():
+    creatureNameImg, nameImg = getImages('Ancient Spawn Of Morgathla')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Angry_Adventurer():
+    creatureNameImg, nameImg = getImages('Angry Adventurer')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Angry_Demon():
+    creatureNameImg, nameImg = getImages('Angry Demon')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Angry_Plant():
+    creatureNameImg, nameImg = getImages('Angry Plant')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Clomp():
+    creatureNameImg, nameImg = getImages('Animated Clomp')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Cyclops():
+    creatureNameImg, nameImg = getImages('Animated Cyclops')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Feather():
+    creatureNameImg, nameImg = getImages('Animated Feather')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Guzzlemaw():
+    creatureNameImg, nameImg = getImages('Animated Guzzlemaw')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Moohtant():
+    creatureNameImg, nameImg = getImages('Animated Moohtant')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Mummy():
+    creatureNameImg, nameImg = getImages('Animated Mummy')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Ogre_Brute():
+    creatureNameImg, nameImg = getImages('Animated Ogre Brute')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Ogre_Savage():
+    creatureNameImg, nameImg = getImages('Animated Ogre Savage')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Ogre_Savage():
+    creatureNameImg, nameImg = getImages('Animated Ogre Savage')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Ogre_Shaman():
+    creatureNameImg, nameImg = getImages('Animated Ogre Shaman')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Rotworm():
+    creatureNameImg, nameImg = getImages('Animated Rotworm')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Skunk():
+    creatureNameImg, nameImg = getImages('Animated Skunk')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Snowman():
+    creatureNameImg, nameImg = getImages('Animated Snowman')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Animated_Sword():
+    creatureNameImg, nameImg = getImages('Animated Sword')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Annihilon():
+    creatureNameImg, nameImg = getImages('Annihilon')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Anomaly():
+    creatureNameImg, nameImg = getImages('Anomaly')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Apprentice_Sheng():
+    creatureNameImg, nameImg = getImages('Apprentice Sheng')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Arachir_The_Ancient_One():
+    creatureNameImg, nameImg = getImages('Arachir The Ancient One')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Arachnophobica():
+    creatureNameImg, nameImg = getImages('Arachnophobica')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Arctic_Faun():
+    creatureNameImg, nameImg = getImages('Arctic Faun')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Armadile():
+    creatureNameImg, nameImg = getImages('Armadile')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Armenius():
+    creatureNameImg, nameImg = getImages('Armenius')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ascending_Ferumbras():
+    creatureNameImg, nameImg = getImages('Ascending Ferumbras')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Ashmunrah():
+    creatureNameImg, nameImg = getImages('Ashmunrah')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Askarak_Demon():
+    creatureNameImg, nameImg = getImages('Askarak Demon')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Askarak_Lord():
+    creatureNameImg, nameImg = getImages('Askarak Lord')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Askarak_Prince():
+    creatureNameImg, nameImg = getImages('Askarak Prince')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Assassin():
+    creatureNameImg, nameImg = getImages('Assassin')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Avalanche():
+    creatureNameImg, nameImg = getImages('Avalanche')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Axeitus_Headbanger():
+    creatureNameImg, nameImg = getImages('Axeitus Headbanger')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
+
+
+def test_should_assert_Azure_Frog():
+    creatureNameImg, nameImg = getImages('Azure Frog')
+    np.testing.assert_array_equal(creatureNameImg, nameImg)
