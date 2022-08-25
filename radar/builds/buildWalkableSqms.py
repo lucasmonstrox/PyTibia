946d89334e148b231ced9cf89553451051b0b8af
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-25 01:17:52 +0100

    :recycle: improve buildWalkableSqms builder

--- radar/builds/buildWalkableSqms.py
+++ radar/builds/buildWalkableSqms.py
@@ -1,18 +1,14 @@
 import numpy as np
 import radar.config
+import radar.core
 import utils.core
 import utils.image
 
 walkableFloorsSqms = np.ndarray(shape=(16, 2048, 2560), dtype=np.uint)
 
-for floor in radar.config["floors"]:
-    floorImg = utils.image.loadAsGrey(
-        'radar/images/floor-{}.png'.format(floor))
-    floorSqms = np.where(
-        np.isin(floorImg, radar.config.nonWalkablePixelsColors),
-        0,
-        1
-    )
+for floor in radar.config.floors:
+    floorImg = utils.image.loadAsGrey(f'radar/images/floor-{floor}.png')
+    floorSqms = np.where(radar.core.isNonWalkablePixelColor(floorImg), 0, 1)
     walkableFloorsSqms[floor] = floorSqms
 
 np.save('radar/npys/walkableFloorsSqms.npy', walkableFloorsSqms)
