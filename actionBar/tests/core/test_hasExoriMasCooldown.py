e41c22b48f45adab02eea70c51f171b573eeea36
lucasmonstro <joao.galiano.silva@gmail.com>
2022-08-30 01:58:41 +0100

    :recycle: add test to cooldowns

---
+++ actionBar/tests/core/test_hasExoriMasCooldown.py
@@ -0,0 +1,18 @@
+import pathlib
+from actionBar.core import hasExoriMasCooldown
+from utils.image import load, RGBtoGray
+
+currentPath = pathlib.Path(__file__).parent.resolve()
+screenshotImg = RGBtoGray(load(f'{currentPath}/screenshot.png'))
+
+
+def test_should_return_True_when_hasCooldownByImg_return_True(mocker):
+    mocker.patch('actionBar.core.hasCooldownByImg', return_value=True)
+    result = hasExoriMasCooldown(screenshotImg)
+    assert result == True
+
+
+def test_should_return_False_when_hasCooldownByImg_return_False(mocker):
+    mocker.patch('actionBar.core.hasCooldownByImg', return_value=False)
+    result = hasExoriMasCooldown(screenshotImg)
+    assert result == False
