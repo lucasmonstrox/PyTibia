71ab1f976a4c398a6dc36c14dbf588b9d16ecbe1
lucasmonstro <joao.galiano.silva@gmail.com>
2022-09-04 02:40:23 +0100

    :label: improve types

--- radar/types.py
+++ radar/types.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-coordinateType = np.dtype([('hash', np.str, 16), ('coordinate', np.uint16, (3,))])
-waypointDistanceType = np.dtype([('index', np.uint16), ('distance', np.float32)])
+coordinateType = np.dtype([('hash', str, 16), ('coordinate', np.uint16, (3,))])
+waypointDistanceType = np.dtype(
+    [('index', np.uint16), ('distance', np.float32)])
 waypointType = np.dtype([
     ('type', np.str_, 64),
     ('coordinate', np.uint32, (3,)),
