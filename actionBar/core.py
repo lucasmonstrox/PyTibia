0b62c59b854b85253532905be0a6494dfc081ab6
lucasmonstro <joao.galiano.silva@gmail.com>
2022-11-23 08:06:06 +0000

    :recycle: improve way to get slot counts

--- actionBar/core.py
+++ actionBar/core.py
@@ -3,42 +3,38 @@ import numpy as np
 from PIL import Image
 from .config import images
 from .extractors import getCooldownsImg
-from .locators import getSlot1Pos, getSlot2Pos, getSlot3Pos, getSlot4Pos, getSlot5Pos, getSlot6Pos, getSlot7Pos, getSlot8Pos, getSlot9Pos
+from .locators import getLeftSideArrowsPos
 from utils.core import locate
-
 
 reader = Reader(['en'])
 
 
-def getSlotCount(screenshot, key):
-    slotPosFunc = {
-        '1': getSlot1Pos,
-        '2': getSlot2Pos,
-        '3': getSlot3Pos,
-        '4': getSlot4Pos,
-        '5': getSlot5Pos,
-        '6': getSlot6Pos,
-        '7': getSlot7Pos,
-        '8': getSlot8Pos,
-        '9': getSlot9Pos,
-    }
-    (x, y, w, _) = slotPosFunc[key](screenshot)
-    x1 = x + w
-    x0 = x1 - 30
-    y0 = y + 22
-    y1 = y0 + 8
-    digits = screenshot[y0:y1, x0:x1]
-    digits = digits[:, 6:30]
+def getSlotCount(screenshot, slot):
+    leftSideArrowsPos = getLeftSideArrowsPos(screenshot)
+    cannotGetLeftSideArrowsPos = leftSideArrowsPos is None
+    if cannotGetLeftSideArrowsPos:
+        return None
+    (xOfLeftSideArrowsPos, yOfLeftSideArrowsPos, widthOfLeftSideArrowsPos, _) = leftSideArrowsPos
+    pageSlot = (slot - 1)
+    slotSize = 34
+    slotGap = 2
+    x0 = xOfLeftSideArrowsPos + widthOfLeftSideArrowsPos + (slot * slotGap) + (pageSlot * slotSize)
+    y0 = yOfLeftSideArrowsPos
+    slotImg = screenshot[y0:y0 + 34, x0:x0 + 34]
+    digits = slotImg[24:32, 3:33]
     basewidth = 24 * 5
     digits = np.where(digits <= 100, 0, digits)
     img = Image.fromarray(digits)
-    wpercent = (basewidth/float(img.size[0]))
+    wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, hsize))
     img = np.array(img)
-    res2 = reader.readtext(img, detail=0)
-    res2 = int(res2[0])
-    return res2
+    textAsArray = reader.readtext(img, detail=0)
+    cannotGetCount = len(textAsArray) == 0
+    if cannotGetCount:
+        return None
+    count = int(textAsArray[0])
+    return count
 
 
 def hasCooldownByImg(screenshot, cooldownImg):
