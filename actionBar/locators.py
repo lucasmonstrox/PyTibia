2a0cb464d9cf351e68d682db924ea71857c3ad76
lucasmonstro <joao.galiano.silva@gmail.com>
2023-02-17 02:28:26 +0000

    :recycle: improve code readability

--- actionBar/locators.py
+++ actionBar/locators.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from utils.core import cacheObjectPos, locate
 from utils.image import loadFromRGBToGray
 
+
 currentPath = Path(__file__).parent.resolve()
 imagesPath = f'{currentPath}/images'
 leftSideArrowsImg = loadFromRGBToGray(f'{imagesPath}/leftSideArrows.png')
