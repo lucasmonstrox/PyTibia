b3bf0e00c99255d86b21f727bd17ea13f1b6ca64
lucasmonstro <joao.galiano.silva@gmail.com>
2023-04-26 02:21:43 +0100

    :bug: fix bug reference when getting target creature

--- src/gameplay/core/tasks/groupOfFollowTargetCreature.py
+++ src/gameplay/core/tasks/groupOfFollowTargetCreature.py
@@ -18,27 +18,28 @@ class GroupOfFollowTargetCreatureTasks(GroupTask):
     # TODO: add typings
     def generateTasks(self, context: Context):
         tasks = np.array([], dtype=Task)
-        if context['cavebot']['targetCreature'] is None:
+        targetCreature = context['cavebot']['targetCreature']
+        if targetCreature is None:
             return tasks
         nonWalkableCoordinates = context['cavebot']['holesOrStairs'].copy()
         for monster in context['gameWindow']['monsters']:
-            if np.array_equal(monster['coordinate'], context['cavebot']['targetCreature']['coordinate']) == False:
+            if np.array_equal(monster['coordinate'], targetCreature['coordinate']) == False:
                 monsterCoordinateTuple = (monster['coordinate'][0], monster['coordinate'][1], monster['coordinate'][2])
                 coordinatesToAppend = np.array([monsterCoordinateTuple], dtype=Coordinate)
                 nonWalkableCoordinates = np.append(nonWalkableCoordinates, coordinatesToAppend)
         gameWindowHeight, gameWindowWidth  = context['gameWindow']['img'].shape
         gameWindowCenter = (gameWindowWidth // 2, gameWindowHeight // 2)
-        monsterGameWindowCoordinate = context['cavebot']['targetCreature']['gameWindowCoordinate']
+        monsterGameWindowCoordinate = targetCreature['gameWindowCoordinate']
         moduleX = abs(gameWindowCenter[0] - monsterGameWindowCoordinate[0])
         moduleY = abs(gameWindowCenter[1] - monsterGameWindowCoordinate[1])
-        dist = distance.cdist([context['radar']['coordinate']], [context['cavebot']['targetCreature']['coordinate']]).flatten()[0]
+        dist = distance.cdist([context['radar']['coordinate']], [targetCreature['coordinate']]).flatten()[0]
         walkpoints = []
         if dist < 2:
             if moduleX > 64 or moduleY > 64:
-                walkpoints.append(context['cavebot']['targetCreature']['coordinate'])
+                walkpoints.append(targetCreature['coordinate'])
         else:
             walkpoints = generateFloorWalkpoints(
-                context['radar']['coordinate'], context['cavebot']['targetCreature']['coordinate'], nonWalkableCoordinates=nonWalkableCoordinates)
+                context['radar']['coordinate'], targetCreature['coordinate'], nonWalkableCoordinates=nonWalkableCoordinates)
             hasWalkpoints = len(walkpoints) > 0
             if hasWalkpoints:
                 walkpoints.pop()
