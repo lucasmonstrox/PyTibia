01412223f9b443da3920893b95039addb4a789ba
lucasmonstro <joao.galiano.silva@gmail.com>
2023-04-28 17:15:58 +0100

    :recycle: improve setMapPlayerStatus middleware

--- src/gameplay/core/middlewares/playerStatus.py
+++ src/gameplay/core/middlewares/playerStatus.py
@@ -1,15 +1,12 @@
 from src.repositories.skills.core import getHp, getMana
 from src.repositories.statusBar.core import getManaPercentage, getHpPercentage
+from ...typings import Context
 
 
-def setMapPlayerStatusMiddleware(context):
-    hpPercentage = getHpPercentage(context['screenshot'])
-    context['statusBar']['hpPercentage'] = hpPercentage
-    hp = getHp(context['screenshot'])
-    context['statusBar']['hp'] = hp
-    manaPercentage = getManaPercentage(context['screenshot'])
-    context['statusBar']['manaPercentage'] = manaPercentage
-    mana = getMana(context['screenshot'])
-    context['statusBar']['mana'] = mana
-    gameContext = context
+# TODO: add unit tests
+def setMapPlayerStatusMiddleware(gameContext: Context) -> Context:
+    gameContext['statusBar']['hp'] = getHp(gameContext['screenshot'])
+    gameContext['statusBar']['hpPercentage'] = getHpPercentage(gameContext['screenshot'])
+    gameContext['statusBar']['mana'] = getMana(gameContext['screenshot'])
+    gameContext['statusBar']['manaPercentage'] = getManaPercentage(gameContext['screenshot'])
     return gameContext
