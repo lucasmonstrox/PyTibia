70b24ab15e2ff0af0a094a11bdb82fc6cf826162
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-08 11:11:44 +0100

    :white_check_mark: add unit tests to OpenLockerTask

--- src/gameplay/core/tasks/openLocker.py
+++ src/gameplay/core/tasks/openLocker.py
@@ -1,6 +1,6 @@
-from src.repositories.gameWindow.core import getSlotFromCoordinate
-from src.repositories.gameWindow.slot import rightClickSlot
-from src.repositories.inventory.core import isLockerOpen
+import src.repositories.gameWindow.core as gameWindowCore
+import src.repositories.gameWindow.slot as gameWindowSlot
+import src.repositories.inventory.core as inventoryCore
 from ...typings import Context
 from .common.base import BaseTask
 
@@ -11,18 +11,14 @@ class OpenLockerTask(BaseTask):
         self.name = 'openLocker'
         self.delayAfterComplete = 1
 
-    # TODO: add unit tests
     def shouldIgnore(self, context: Context) -> bool:
-        shouldIgnoreTask = isLockerOpen(context['screenshot'])
+        shouldIgnoreTask = inventoryCore.isLockerOpen(context['screenshot'])
         return shouldIgnoreTask
 
-    # TODO: add unit tests
     def do(self, context: Context) -> Context:
-        slot = getSlotFromCoordinate(context['radar']['coordinate'], context['deposit']['lockerCoordinate'])
-        rightClickSlot(slot, context['gameWindow']['coordinate'])
+        slot = gameWindowCore.getSlotFromCoordinate(context['radar']['coordinate'], context['deposit']['lockerCoordinate'])
+        gameWindowSlot.rightClickSlot(slot, context['gameWindow']['coordinate'])
         return context
 
-    # TODO: add unit tests
     def did(self, context: Context) -> bool:
-        didTask = isLockerOpen(context['screenshot'])
-        return didTask
+        return self.shouldIgnore(context)
---
+++ tests/unit/gameplay/core/tasks/test_openLocker.py
@@ -0,0 +1,50 @@
+from src.gameplay.core.tasks.openLocker import OpenLockerTask
+
+
+def test_should_test_default_params():
+    task = OpenLockerTask()
+    assert task.name == 'openLocker'
+    assert task.delayAfterComplete == 1
+
+def test_should_method_shouldIgnore_return_False_when_isLockerOpen_return_False(mocker):
+    context = {'screenshot': []}
+    task = OpenLockerTask()
+    isLockerOpenSpy = mocker.patch('src.repositories.inventory.core.isLockerOpen', return_value=False)
+    assert task.shouldIgnore(context) == False
+    isLockerOpenSpy.assert_called_once_with(context['screenshot'])
+
+def test_should_method_shouldIgnore_return_True_when_isLockerOpen_return_True(mocker):
+    context = {'screenshot': []}
+    task = OpenLockerTask()
+    isLockerOpenSpy = mocker.patch('src.repositories.inventory.core.isLockerOpen', return_value=True)
+    assert task.shouldIgnore(context) == True
+    isLockerOpenSpy.assert_called_once_with(context['screenshot'])
+
+def test_should_do(mocker):
+    context = {
+        'deposit': {'lockerCoordinate': [1, 2, 3]},
+        'gameWindow': {'coordinate': [4, 5, 6]},
+        'radar': {'coordinate': [7, 8, 9]}
+    }
+    task = OpenLockerTask()
+    slot = (0, 0)
+    getSlotFromCoordinateSpy = mocker.patch('src.repositories.gameWindow.core.getSlotFromCoordinate', return_value=slot)
+    rightClickSlotSpy = mocker.patch('src.repositories.gameWindow.slot.rightClickSlot')
+    assert task.do(context) == context
+    getSlotFromCoordinateSpy.assert_called_once_with(context['radar']['coordinate'], context['deposit']['lockerCoordinate'])
+    rightClickSlotSpy.assert_called_once_with(slot, context['gameWindow']['coordinate'])
+
+def test_should_method_did_return_False_when_shouldIgnore_return_False(mocker):
+    context = {'screenshot': []}
+    task = OpenLockerTask()
+    shouldIgnoreSpy = mocker.patch.object(task, 'shouldIgnore', return_value=False)
+    assert task.did(context) == False
+    shouldIgnoreSpy.assert_called_once_with(context)
+
+def test_should_method_did_return_True_when_shouldIgnore_return_True(mocker):
+    context = {'screenshot': []}
+    task = OpenLockerTask()
+    shouldIgnoreSpy = mocker.patch.object(task, 'shouldIgnore', return_value=True)
+    assert task.did(context) == True
+    shouldIgnoreSpy.assert_called_once_with(context)
+
