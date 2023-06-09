348e8a09d08253b88af2bfe37d03beeb87a16160
Raphael Miranda Rezende <raphaelrezende@fitec.org.br>
2023-06-09 01:49:54 -0300

    :recycle: remove blank lines

--- src/gameplay/core/middlewares/window.py
+++ src/gameplay/core/middlewares/window.py
@@ -7,14 +7,10 @@ def setTibiaWindowMiddleware(gameContext: Context):
     if gameContext['window'] is None:
         windowsList: list = []
         win32gui.EnumWindows(lambda hwnd, param: param.append(hwnd), windowsList)
-        windowsNames = list(
-            map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList)
-        )
+        windowsNames = list(map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList))
         regex = re.compile(r'Tibia - .*')
 
-        windowsFilter = list(
-            filter(lambda windowName: regex.match(windowName), windowsNames)
-        )
+        windowsFilter = list(filter(lambda windowName: regex.match(windowName), windowsNames))
         if len(windowsFilter) > 0:
             gameContext['window'] = windowsFilter[0]
     
