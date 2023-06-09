96a65408d7ca839a7420d6b80ab74eb94da3a7ca
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-09 08:01:10 +0100

    refactor: add missing types

--- src/utils/mouse.py
+++ src/utils/mouse.py
@@ -1,32 +1,24 @@
 import pyautogui
+from src.shared.typings import XYCoordinate
 
 
-# TODO: add types
 # TODO: add unit tests
-def drag(x1y1, x2y2):
+def drag(x1y1: XYCoordinate, x2y2: XYCoordinate):
     pyautogui.moveTo(x1y1[0], x1y1[1])
     pyautogui.dragTo(x2y2[0], x2y2[1], button='left')
 
 
-# TODO: add types
 # TODO: add unit tests
-def leftClick(windowCoordinate=None):
+def leftClick(windowCoordinate: XYCoordinate = None):
     if windowCoordinate is None:
         pyautogui.leftClick()
         return
     pyautogui.leftClick(windowCoordinate[0], windowCoordinate[1])
 
 
-# TODO: add types
 # TODO: add unit tests
-def move(windowCoordinate):
+def moveTo(windowCoordinate: XYCoordinate):
     pyautogui.moveTo(windowCoordinate[0], windowCoordinate[1])
-
-
-# TODO: add types
-# TODO: add unit tests
-def moveTo(windowCoordinate):
-    pyautogui.moveTo(windowCoordinate)
 
 
 # TODO: add unit tests
@@ -34,9 +26,8 @@ def scroll(clicks: int):
     pyautogui.scroll(clicks)
 
 
-# TODO: add types
 # TODO: add unit tests
-def rightClick(windowCoordinate=None):
+def rightClick(windowCoordinate: XYCoordinate = None):
     if windowCoordinate is None:
         pyautogui.rightClick()
         return
