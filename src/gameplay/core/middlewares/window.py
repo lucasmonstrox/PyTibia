02a02fc592a08b5b271870d25013e20b1c485006
Raphael Miranda Rezende <raphaelrezende@fitec.org.br>
2023-06-09 01:13:29 -0300

    :truck: get automatic tibia window

--- src/gameplay/core/middlewares/window.py
+++ src/gameplay/core/middlewares/window.py
@@ -1,7 +1,23 @@
 import win32gui
+import re
+from ...typings import Context
 
 
-def setTibiaWindowMiddleware(gameContext):
-    if gameContext['window'] is None:
-        gameContext['window'] = win32gui.FindWindow(None, 'Tibia - Lucas Monstro')
-    return gameContext
+def setTibiaWindowMiddleware(gameContext: Context):
+    '''
+        Windows middleware to set the Tibia window
+    '''
+    windowsList: list = []
+    win32gui.EnumWindows(lambda hwnd, param: param.append(hwnd), windowsList) # hwnd -> window handler
+    windowsNames = list(
+        map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList)
+    )
+    regex = re.compile(r'Tibia - .*')
+
+    gameContext['window'] = list(
+        filter(lambda window_name: regex.match(window_name), windowsNames)
+    )
+    if len(gameContext['window']) > 0: # if have more than one window, get the first
+        gameContext['window'] = gameContext['window'][0]
+        return gameContext
+    raise Exception('Tibia window not found')
