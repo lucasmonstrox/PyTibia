16bbbc93031b75f0b0f65defb6f1252b93a489d6
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-09 08:09:11 +0100

    refactor: improve perf

--- src/repositories/radar/extractors.py
+++ src/repositories/radar/extractors.py
@@ -5,11 +5,8 @@ from src.shared.typings import BBox, GrayImage
 # TODO: add unit tests
 # TODO: add perf
 def getRadarImage(screenshot: GrayImage, radarToolsPosition: BBox) -> GrayImage:
-    radarToolsPositionX = radarToolsPosition[0]
-    radarToolsPositionY = radarToolsPosition[1]
-    x0 = radarToolsPositionX - config.dimensions['width'] - 11
+    x0 = radarToolsPosition[0] - config.dimensions['width'] - 11
     x1 = x0 + config.dimensions['width']
-    y0 = radarToolsPositionY - 50
+    y0 = radarToolsPosition[1] - 50
     y1 = y0 + config.dimensions['height']
-    radarImage = screenshot[y0:y1, x0:x1]
-    return radarImage
+    return screenshot[y0:y1, x0:x1]
