49479006677c5767a93f57ba23ac3ca852e0c434
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-09 08:00:39 +0100

    refactor: remove constructor param

--- src/gameplay/core/tasks/pressLogoutKeys.py
+++ src/gameplay/core/tasks/pressLogoutKeys.py
@@ -4,13 +4,11 @@ from .common.base import BaseTask
 
 
 class PressLogoutKeys(BaseTask):
-    # TODO: add types
-    def __init__(self, keys):
+    def __init__(self):
         super().__init__()
         self.name = 'pressKeys'
         self.delayBeforeStart = 1
         self.delayAfterComplete = 1
-        self.keys = keys
 
     # TODO: add unit tests
     def do(self, context: Context) -> Context:
