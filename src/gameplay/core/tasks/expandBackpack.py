1e204bb4eea0c01f727c0731fa25eddc90c6b2ad
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-10 21:00:05 +0100

    refactor: improve clean code

--- src/gameplay/core/tasks/expandBackpack.py
+++ src/gameplay/core/tasks/expandBackpack.py
@@ -1,12 +1,13 @@
 from src.gameplay.typings import Context
 from src.repositories.inventory.core import images
-from src.shared.typings import GrayImage
 from src.utils.core import locate
 from src.utils.mouse import drag
 from ...typings import Context
 from .common.base import BaseTask
 
 
+# TODO: ignore when backpack already expanded
+# TODO: check if backpack is expanded on did
 class ExpandBackpackTask(BaseTask):
     def __init__(self, backpack: str):
         super().__init__()
@@ -16,17 +17,13 @@ class ExpandBackpackTask(BaseTask):
         self.terminable = False
         self.backpack = backpack
 
-    # TODO: ignore if backpack already expanded
-    # TODO: add unit tests
-    def shouldIgnore(self, context: Context) -> bool:
-        return super().shouldIgnore(context)
-
     # TODO: add unit tests
     def do(self, context: Context) -> Context:
         backpackBarImage = images['containersBars'][context['backpacks']['main']]
         # TODO: locate should be done in right content position to avoid calculation in whole screen
         backpackBarPosition = locate(context['screenshot'], backpackBarImage, confidence=0.8)
         croppedImage = context['screenshot'][backpackBarPosition[1]:, backpackBarPosition[0]:]
+        # TODO: locate should be done in right content position to avoid calculation in whole screen
         backpackBottomBarPosition = locate(croppedImage, images['containersBars']['backpack bottom'], confidence=0.8)
         backpackBottomBarPositionX = backpackBottomBarPosition[0] + backpackBarPosition[0]
         backpackBottomBarPositionY = backpackBottomBarPosition[1] + backpackBarPosition[1]
@@ -37,8 +34,3 @@ class ExpandBackpackTask(BaseTask):
             return context
         drag((backpackBottomBarPositionX, backpackBottomBarPositionY), (backpackBottomBarPositionX, backpackBottomBarPositionY + scrollY))
         return context
-
-    # TODO: check if backpack is expanded
-    # TODO: add unit tests
-    def did(self, _: Context) -> bool:
-        return True
