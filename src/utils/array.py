d82bf99fb00f0c4902299e1133cbb25e0e9e4c49
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-12 07:33:37 +0100

    refactor: remove unused TODO

--- src/utils/array.py
+++ src/utils/array.py
@@ -1,6 +1,5 @@
 from typing import List, TypeVar
 
 
-# TODO: add unit tests
 def getNextArrayIndex(items: List[TypeVar('T')], currentIndex: int) -> int:
     return currentIndex + 1 if currentIndex < len(items) - 1 else 0
