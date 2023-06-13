f7cb1e53a4b9e33c1c0283026ae97f58162c45f6
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-13 01:14:39 +0100

    perf: improve code performance

--- src/repositories/actionBar/core.py
+++ src/repositories/actionBar/core.py
@@ -7,7 +7,6 @@ import src.utils.core as coreUtils
 from .config import images
 
 
-
 # TODO: add unit tests
 def getSlotCount(screenshot: GrayImage, slot: int) -> Union[int, None]:
     leftSideArrowsPos = actionBarLocators.getLeftArrowsPosition(screenshot)
@@ -37,8 +36,7 @@ def hasCooldownByImage(screenshot: GrayImage, cooldownImage: GrayImage) -> Union
     cooldownImagePosition = coreUtils.locate(listOfCooldownsImage, cooldownImage)
     if cooldownImagePosition is None:
         return False
-    (x, _, width, _) = cooldownImagePosition
-    percentBar = listOfCooldownsImage[20:21, x:x + width]
+    percentBar = listOfCooldownsImage[20:21, cooldownImagePosition[0]:cooldownImagePosition[0] + cooldownImagePosition[2]]
     return percentBar[0][0] == 255
 
 
@@ -98,15 +96,13 @@ def slotIsEquipped(screenshot: GrayImage, slot: int) -> Union[bool, None]:
     x0 = xOfLeftSideArrowsPos + widthOfLeftSideArrowsPos + (slot * 2) + ((slot - 1) * 34)
     y0 = yOfLeftSideArrowsPos
     slotImage = screenshot[y0:y0 + 34, x0:x0 + 34]
-    isEquipped = slotImage[0, 0] == 41
-    return isEquipped
+    return slotImage[0, 0] == 41
 
 
 def slotIsAvailable(screenshot: GrayImage, slot: int) -> Union[bool, None]:
     leftSideArrowsPos = actionBarLocators.getLeftArrowsPosition(screenshot)
     if leftSideArrowsPos is None:
         return None
-    (xOfLeftSideArrowsPos, yOfLeftSideArrowsPos, widthOfLeftSideArrowsPos, _) = leftSideArrowsPos
-    x0 = xOfLeftSideArrowsPos + widthOfLeftSideArrowsPos + (slot * 2) + ((slot - 1) * 34)
-    slotImage = screenshot[yOfLeftSideArrowsPos:yOfLeftSideArrowsPos + 34, x0:x0 + 34]
+    x0 = leftSideArrowsPos[0] + leftSideArrowsPos[2] + (slot * 2) + ((slot - 1) * 34)
+    slotImage = screenshot[leftSideArrowsPos[1]:leftSideArrowsPos[1] + 34, x0:x0 + 34]
     return not (slotImage[1, 2] == 54 and slotImage[1, 4] == 54 and slotImage[1, 6] == 54 and slotImage[1, 8] == 54 and slotImage[1, 10] == 54)
