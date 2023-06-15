77777d9ee6644d72b018f328bcb5b46c90a3f0cb
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-15 01:46:24 +0100

    perf: improve performance getting mana/hp percent

--- src/repositories/statusBar/core.py
+++ src/repositories/statusBar/core.py
@@ -1,7 +1,7 @@
-import numpy as np
+from numba import njit
 from typing import Union
 from src.shared.typings import GrayImage
-from .config import barSize, hpBarAllowedPixelsColors, manaBarAllowedPixelsColors
+from .config import hpBarAllowedPixelsColors, manaBarAllowedPixelsColors
 from .extractors import getHpBar, getManaBar
 from .locators import getHpIconPosition, getManaIconPosition
 
@@ -10,10 +10,13 @@ from .locators import getHpIconPosition, getManaIconPosition
 # TODO: add unit tests
 # TODO: add perf
 # TODO: change to numba to increase performance
-def getFilledBarPercentage(bar, size: int=100, allowedPixelsColors=[]) -> int:
-    bar = np.where(np.isin(bar, allowedPixelsColors), 0, bar)
-    barPercent = np.count_nonzero(bar == 0)
-    return (barPercent * 100 // size)
+@njit(cache=True, fastmath=True)
+def getFilledBarPercentage(bar, allowedPixelsColors=[]) -> int:
+    barPercent = len(bar)
+    for i in range(len(bar)):
+        if bar[i] not in allowedPixelsColors:
+            barPercent -= 1
+    return (barPercent * 100 // 94)
 
 
 # TODO: add unit tests
@@ -23,8 +26,7 @@ def getHpPercentage(screenshot: GrayImage) -> Union[int, None]:
     if hpIconPosition == None:
         return None
     bar = getHpBar(screenshot, hpIconPosition)
-    return getFilledBarPercentage(bar, size=barSize,
-                                     allowedPixelsColors=hpBarAllowedPixelsColors)
+    return getFilledBarPercentage(bar, allowedPixelsColors=hpBarAllowedPixelsColors)
 
 
 # TODO: add unit tests
@@ -34,5 +36,4 @@ def getManaPercentage(screenshot: GrayImage) -> Union[int, None]:
     if manaIconPosition == None:
         return None
     bar = getManaBar(screenshot, manaIconPosition)
-    return getFilledBarPercentage(bar, size=barSize,
-                                     allowedPixelsColors=manaBarAllowedPixelsColors)
+    return getFilledBarPercentage(bar, allowedPixelsColors=manaBarAllowedPixelsColors)
