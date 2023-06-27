49fdb5775160b6eb0d41a19772b85e6f7ba204bd
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-27 18:32:42 +0100

    refactor: remove kivy stuffs

--- src/ui/screens/setup.py
+++
@@ -1,8 +0,0 @@
-from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import MDScreen
-
-
-class SetupScreen(MDScreen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.add_widget(MDLabel(text='Tela de setup'))
