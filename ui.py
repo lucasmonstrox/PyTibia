27cd98a811ec3b1522841aff754fafa66fb6f78a
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-27 20:14:42 +0100

    refactor: remove unused file

--- ui.py
+++
@@ -1,8 +0,0 @@
-from src.gameplay.context import context
-from src.ui.application import Application
-from src.ui.context import Context
-
-
-if __name__ == "__main__":
-    app = Application(Context(context))
-    app.mainloop()
