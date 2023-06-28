19f186b006a7c7f6b30833b577c9fc391ce32b5f
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-28 02:58:22 +0100

    refactor: add return type

--- src/gameplay/core/middlewares/window.py
+++ src/gameplay/core/middlewares/window.py
@@ -4,13 +4,16 @@ from ...typings import Context
 
 
 # TODO: add unit tests
-def setTibiaWindowMiddleware(context: Context):
+def setTibiaWindowMiddleware(context: Context) -> Context:
     if context['window'] is None:
         windowsList: list = []
-        win32gui.EnumWindows(lambda hwnd, param: param.append(hwnd), windowsList)
-        windowsNames = list(map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList))
+        win32gui.EnumWindows(
+            lambda hwnd, param: param.append(hwnd), windowsList)
+        windowsNames = list(
+            map(lambda hwnd: win32gui.GetWindowText(hwnd), windowsList))
         regex = re.compile(r'Tibia - .*')
-        windowsFilter = list(filter(lambda windowName: regex.match(windowName), windowsNames))
+        windowsFilter = list(
+            filter(lambda windowName: regex.match(windowName), windowsNames))
         if len(windowsFilter) > 0:
             context['window'] = windowsFilter[0]
     return context
