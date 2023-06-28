7a138284b438050fadb52c7d1ae5e81460b6d9c1
lucasmonstro <joao.galiano.silva@gmail.com>
2023-06-28 02:48:03 +0100

    refactor: remove unused types

--- src/gameplay/core/typings.py
+++ src/gameplay/core/typings.py
@@ -1,10 +1,6 @@
 from typing_extensions import TypedDict
 from src.shared.typings import Coordinate
-import numpy as np
 
 
-Checkpoint = TypedDict('Checkpoint', {'goalCoordinate': Coordinate, 'checkInCoordinate': Coordinate})
-Task = np.dtype([
-    ('type', np.str_, 64),
-    ('data', np.object_),
-])
+Checkpoint = TypedDict(
+    'Checkpoint', {'goalCoordinate': Coordinate, 'checkInCoordinate': Coordinate})
