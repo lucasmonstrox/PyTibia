dea036acd607a9b148e00e4af578e42602685882
lucasmonstro <joao.galiano.silva@gmail.com>
2023-07-01 01:31:26 +0100

    refactor: fix types

--- src/utils/array.py
+++ src/utils/array.py
@@ -1,5 +1,5 @@
 from typing import List, TypeVar
 
 
-def getNextArrayIndex(items: List[TypeVar['T']], currentIndex: int) -> int:
+def getNextArrayIndex(items: List[TypeVar('T')], currentIndex: int) -> int:
     return currentIndex + 1 if currentIndex < len(items) - 1 else 0
