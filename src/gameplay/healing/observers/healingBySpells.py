b40a15cccd7c6a6e1b9ee6b5d8fb53b0222d66af
lucasmonstro <joao.galiano.silva@gmail.com>
2023-07-04 14:39:17 +0100

    refactor: add missing spells

--- src/gameplay/healing/observers/healingBySpells.py
+++ src/gameplay/healing/observers/healingBySpells.py
@@ -18,17 +18,31 @@ def healingBySpellsObserver(context: Context):
             return
     if context['healing']['spells']['criticalHealing']['enabled']:
         if context['statusBar']['hpPercentage'] <= context['healing']['spells']['criticalHealing']['hpPercentageLessThanOrEqual'] and not hasCooldownByName(context['screenshot'], context['healing']['spells']['criticalHealing']['spell']['name']):
-            tasksOrchestrator.setRootTask(context, UseHotkeyTask(context['healing']['spells']['criticalHealing']['hotkey']))
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['criticalHealing']['hotkey']))
+            return
+    if context['healing']['spells']['criticalHealing']['enabled']:
+        if context['statusBar']['hpPercentage'] <= context['healing']['spells']['mediumHealing']['hpPercentageLessThanOrEqual'] and not hasCooldownByName(context['screenshot'], context['healing']['spells']['mediumHealing']['spell']['name']):
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['mediumHealing']['hotkey']))
             return
     if context['healing']['spells']['lightHealing']['enabled']:
         if context['statusBar']['hpPercentage'] <= context['healing']['spells']['lightHealing']['hpPercentageLessThanOrEqual'] and not hasCooldownByName(context['screenshot'], context['healing']['spells']['lightHealing']['spell']['name']):
-            tasksOrchestrator.setRootTask(context, UseHotkeyTask(context['healing']['spells']['lightHealing']['hotkey']))
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['lightHealing']['hotkey']))
+            return
+    if context['healing']['spells']['uturaGran']['enabled']:
+        if context['statusBar']['mana'] >= context['healing']['spells']['uturaGran']['spell']['manaNeeded'] and not hasCooldownByName(context['screenshot'], 'utura gran'):
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['uturaGran']['hotkey']))
             return
     if context['healing']['spells']['utura']['enabled']:
-        if context['statusBar']['mana'] >= context['healing']['spells']['utura']['spell']['manaNeeded'] and not hasCooldownByName(context['screenshot'], context['healing']['spells']['utura']['spell']['name']):
-            tasksOrchestrator.setRootTask(context, UseHotkeyTask(context['healing']['spells']['utura']['hotkey']))
+        if context['statusBar']['mana'] >= context['healing']['spells']['utura']['spell']['manaNeeded'] and not hasCooldownByName(context['screenshot'], 'utura'):
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['utura']['hotkey']))
             return
     if context['healing']['spells']['exuraGranIco']['enabled']:
         if context['statusBar']['mana'] >= context['healing']['spells']['exuraGranIco']['spell']['manaNeeded'] and not hasCooldownByName(context['screenshot'], 'exura gran ico'):
-            tasksOrchestrator.setRootTask(context, UseHotkeyTask(context['healing']['spells']['exuraGranIco']['hotkey']))
+            tasksOrchestrator.setRootTask(context, UseHotkeyTask(
+                context['healing']['spells']['exuraGranIco']['hotkey']))
             return
