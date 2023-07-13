aa6c7d9dcb9e216acd64a2d3d520e3b7d971ce64
lucasmonstro <joao.galiano.silva@gmail.com>
2023-07-13 03:25:20 +0100

    test: refactor hotkeys

--- tests/unit/gameplay/core/tasks/test_useRope.py
+++ tests/unit/gameplay/core/tasks/test_useRope.py
@@ -9,19 +9,22 @@ def test_should_test_default_params():
     assert task.delayAfterComplete == 1
     assert task.waypoint == waypoint
 
+
 def test_should_do(mocker):
     context = {
         'gameWindow': {'coordinate': (1, 2, 3)},
-        'hotkeys': {'rope': 'f8'},
         'radar': {'coordinate': (4, 5, 6)},
     }
     waypoint = {'coordinate': (7, 8, 9)}
     task = UseRopeTask(waypoint)
     slot = (0, 0)
-    getSlotFromCoordinateSpy = mocker.patch('src.repositories.gameWindow.core.getSlotFromCoordinate', return_value=slot)
+    getSlotFromCoordinateSpy = mocker.patch(
+        'src.repositories.gameWindow.core.getSlotFromCoordinate', return_value=slot)
     clickSlotSpy = mocker.patch('src.repositories.gameWindow.slot.clickSlot')
     pressSpy = mocker.patch('src.utils.keyboard.press')
     assert task.do(context) == context
-    getSlotFromCoordinateSpy.assert_called_once_with(context['radar']['coordinate'], waypoint['coordinate'])
-    clickSlotSpy.assert_called_once_with(slot, context['gameWindow']['coordinate'])
-    pressSpy.assert_called_once_with(context['hotkeys']['rope'])
+    getSlotFromCoordinateSpy.assert_called_once_with(
+        context['radar']['coordinate'], waypoint['coordinate'])
+    clickSlotSpy.assert_called_once_with(
+        slot, context['gameWindow']['coordinate'])
+    pressSpy.assert_called_once_with('o')
--- tests/unit/gameplay/core/tasks/test_useShovel.py
+++ tests/unit/gameplay/core/tasks/test_useShovel.py
@@ -4,12 +4,14 @@ from src.gameplay.core.tasks.useShovel import UseShovelTask
 
 waypoint = {'coordinate': (0, 0, 0)}
 
+
 def test_should_test_default_params():
     task = UseShovelTask(waypoint)
     assert task.name == 'useShovel'
     assert task.delayBeforeStart == 1
     assert task.delayAfterComplete == 0.5
     assert task.waypoint == waypoint
+
 
 def test_should_method_shouldIgnore_return_False_when_hole_is_not_open(mocker):
     context = {
@@ -18,9 +20,12 @@ def test_should_method_shouldIgnore_return_False_when_hole_is_not_open(mocker):
         'radar': {'coordinate': (4, 5, 6)},
     }
     task = UseShovelTask(waypoint)
-    isHoleOpenSpy = mocker.patch('src.repositories.gameWindow.core.isHoleOpen', return_value=False)
+    isHoleOpenSpy = mocker.patch(
+        'src.repositories.gameWindow.core.isHoleOpen', return_value=False)
     assert task.shouldIgnore(context) == False
-    isHoleOpenSpy.assert_called_once_with(context['gameWindow']['image'], gameWindowCore.images[context['resolution']]['holeOpen'], context['radar']['coordinate'], waypoint['coordinate'])
+    isHoleOpenSpy.assert_called_once_with(context['gameWindow']['image'], gameWindowCore.images[context['resolution']]
+                                          ['holeOpen'], context['radar']['coordinate'], waypoint['coordinate'])
+
 
 def test_should_method_shouldIgnore_return_False_when_hole_is_open(mocker):
     context = {
@@ -29,26 +34,33 @@ def test_should_method_shouldIgnore_return_False_when_hole_is_open(mocker):
         'radar': {'coordinate': (4, 5, 6)},
     }
     task = UseShovelTask(waypoint)
-    isHoleOpenSpy = mocker.patch('src.repositories.gameWindow.core.isHoleOpen', return_value=True)
+    isHoleOpenSpy = mocker.patch(
+        'src.repositories.gameWindow.core.isHoleOpen', return_value=True)
     assert task.shouldIgnore(context) == True
-    isHoleOpenSpy.assert_called_once_with(context['gameWindow']['image'], gameWindowCore.images[context['resolution']]['holeOpen'], context['radar']['coordinate'], waypoint['coordinate'])
+    isHoleOpenSpy.assert_called_once_with(context['gameWindow']['image'], gameWindowCore.images[context['resolution']]
+                                          ['holeOpen'], context['radar']['coordinate'], waypoint['coordinate'])
+
 
 def test_should_do(mocker):
     context = {
         'gameWindow': {'coordinate': (4, 5, 6), 'image': []},
-        'hotkeys': {'shovel': 'f8'},
         'resolution': 1080,
         'radar': {'coordinate': (4, 5, 6)},
     }
     slot = (0, 0)
     task = UseShovelTask(waypoint)
-    getSlotFromCoordinateSpy = mocker.patch('src.repositories.gameWindow.core.getSlotFromCoordinate', return_value=slot)
+    getSlotFromCoordinateSpy = mocker.patch(
+        'src.repositories.gameWindow.core.getSlotFromCoordinate', return_value=slot)
     pressSpy = mocker.patch('src.utils.keyboard.press')
-    clickSlotSpy = mocker.patch('src.repositories.gameWindow.slot.clickSlot', return_value=True)
+    clickSlotSpy = mocker.patch(
+        'src.repositories.gameWindow.slot.clickSlot', return_value=True)
     assert task.do(context) == context
-    getSlotFromCoordinateSpy.assert_called_once_with(context['radar']['coordinate'], waypoint['coordinate'])
-    pressSpy.assert_called_once_with(context['hotkeys']['shovel'])
-    clickSlotSpy.assert_called_once_with(slot, context['gameWindow']['coordinate'])
+    getSlotFromCoordinateSpy.assert_called_once_with(
+        context['radar']['coordinate'], waypoint['coordinate'])
+    pressSpy.assert_called_once_with('p')
+    clickSlotSpy.assert_called_once_with(
+        slot, context['gameWindow']['coordinate'])
+
 
 def test_should_method_did_return_False_when_shouldIgnore_return_False(mocker):
     context = {
@@ -57,9 +69,11 @@ def test_should_method_did_return_False_when_shouldIgnore_return_False(mocker):
         'radar': {'coordinate': (4, 5, 6)},
     }
     task = UseShovelTask(waypoint)
-    shouldIgnoreSpy = mocker.patch.object(task, 'shouldIgnore', return_value=False)
+    shouldIgnoreSpy = mocker.patch.object(
+        task, 'shouldIgnore', return_value=False)
     assert task.did(context) == False
     shouldIgnoreSpy.assert_called_once_with(context)
+
 
 def test_should_method_did_return_True_when_shouldIgnore_return_True(mocker):
     context = {
@@ -68,6 +82,7 @@ def test_should_method_did_return_True_when_shouldIgnore_return_True(mocker):
         'radar': {'coordinate': (4, 5, 6)},
     }
     task = UseShovelTask(waypoint)
-    shouldIgnoreSpy = mocker.patch.object(task, 'shouldIgnore', return_value=True)
+    shouldIgnoreSpy = mocker.patch.object(
+        task, 'shouldIgnore', return_value=True)
     assert task.did(context) == True
     shouldIgnoreSpy.assert_called_once_with(context)
